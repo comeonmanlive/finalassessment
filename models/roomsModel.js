e579388e9bdaf93dc419cd0d28e41778c0fadfce
comeonmanlive <francisco.frederico2002@gmail.com>
2022-06-04 14:30:37 +0100

    First Part

--- models/roomsModel.js
+++ models/roomsModel.js
@@ -1,4 +1,5 @@
 var pool = require('./connection.js')
+var beats = { rock: 'scissors', scissors: 'paper', paper: 'rock' };
 
 module.exports.getAllRooms = async function() {
   try {
@@ -11,3 +12,59 @@ module.exports.getAllRooms = async function() {
     return { status: 500, result: err};
   }
 }  
+module.exports.getRoomById = async function (id) {
+  try {
+    let sql = "Select * from room where roo_id = $1";
+    let result = await pool.query(sql, [id]);
+    if (result.rows.length > 0) {
+      let room = result.rows[0];
+      return { status: 200, result: room };
+    } else {
+      return { status: 404, result: { msg: "No room with that id" } };
+    }
+  } catch (err) {
+    console.log(err);
+    return { status: 500, result: err };
+  }
+}
+module.exports.play = async function (id, value) {
+  try {
+    if (!beats[value]) {
+      return { status: 400, result: { msg: "Card value is not valid (rock,paper,scissors)" } };
+    }
+    let sql = "Select * from room where roo_id = $1";
+    let result = await pool.query(sql, [id]);
+    if (result.rows.length == 0) {
+      return { status: 404, result: { msg: "No room with that id" } };
+    }
+    let room = result.rows[0];
+    if (beats[value] != room.roo_topcard.toLowerCase()) {
+      return {
+        status: 200,
+        result: {
+          victory: false,
+          msg: "You Lost! That card does not beat the top card.",
+          current_topcard: room.roo_topcard
+        }
+      };
+    }
+    let sql2 = "UPDATE room SET roo_topcard = $1 WHERE roo_id = $2";
+    let result2 = await pool.query(sql2, [value, id]);
+    if (result2.rowCount == 0) {
+      return { status: 500, 
+                result: { msg: "Not able to update. Many possible reasons (ex: room was deleted during play)" } };
+    }
+    return {
+      status: 200,
+      result: {
+        victory: true,
+        msg: "You Won!",
+        current_topcard: value
+      }
+    };
+
+  } catch (err) {
+    console.log(err);
+    return { status: 500, result: err };
+  }
+}
--- routes/roomsRoutes.js
+++ routes/roomsRoutes.js
@@ -6,5 +6,19 @@ router.get('/', async function(req, res, next) {
     let result = await rModel.getAllRooms();
     res.status(result.status).send(result.result);
 });
-            
+router.get('/:id', async function(req, res, next) {
+    let id = req.params.id;
+    console.log("Get room with id "+id)
+    let result = await rModel.getRoomById(id);
+    res.status(result.status).send(result.result);
+  });      
+
+  router.post('/:id/plays', async function(req, res, next) {
+    let id = req.params.id;
+    let cardPlayed = req.body.cardPlayed.toLowerCase();
+    console.log(`Played card ${cardPlayed} on room with id ${id}`);
+    let result = await rModel.play(id,cardPlayed);
+    res.status(result.status).send(result.result);
+  });
+        
 module.exports = router;
